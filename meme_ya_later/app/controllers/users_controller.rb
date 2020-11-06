913a2643048d07e24de5e89bebf8fd9a470d668d
JakesProjects1 <jacob@kagon.net>
2020-11-06 17:27:03 -0500

    updates

--- meme_ya_later/app/controllers/users_controller.rb
+++ meme_ya_later/app/controllers/users_controller.rb
@@ -6,5 +6,13 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.create(params.require(:user).permit(:name, :username,        
+      :email, :password))
+      session[:user_id] = @user.id
+      redirect_to '/welcome'
+   end
 end
