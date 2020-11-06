ea179c88ed3cf34508a68cfad318d72741a528d9
JakesProjects1 <jacob@kagon.net>
2020-11-06 11:34:15 -0500

    updates

--- meme_ya_later/app/models/user.rb
+++ meme_ya_later/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_many :senders, foreign_key: :sender_id , class_name: "Message"
-    has_many :receivers, through: :senders
-    has_many :receivers, foreign_key: :receiver_id, class_name: "Message"
-    has_many :senders, through: :receivers
+    has_many :sent_messages, class_name: "Message", foreign_key: :sender_id 
+    has_many :receivers, through: :sent_messages, source: :receiver
+    has_many :received_messages, class_name: "Message", foreign_key: :receiver_id 
+    has_many :senders, through: :received_messages, source: :sender
     has_many :memes
 end
