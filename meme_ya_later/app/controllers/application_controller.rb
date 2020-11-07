3a906325d1ff47119ead9b3d4eea6a3b7da2e8a5
JakesProjects1 <jacob@kagon.net>
2020-11-06 22:29:33 -0500

    updates to login form

--- meme_ya_later/app/controllers/application_controller.rb
+++ meme_ya_later/app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    helper_method :logged_in?
+    
     def home
         
     end
+
+    def current_user    
+        User.find_by(id: session[:user_id])  
+    end
+
+    def logged_in?
+       
+        !current_user.nil?  
+    end
+
+
 end
+
+
--- meme_ya_later/app/controllers/users_controller.rb
+++ meme_ya_later/app/controllers/users_controller.rb
@@ -12,12 +12,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
-    if @user && @user.authenticate(params[:user][:password])
-       session[:user_id] = @user.id
-       redirect_to user_path(@user)
-    else
-      redirect_to new_session_path
+    
     create(params.require(:user).permit(:name, :username,        
       :email, :password_digest))
       session[:user_id] = @user.id
