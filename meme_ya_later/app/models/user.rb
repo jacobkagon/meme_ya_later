57c25af4671a8586ee4c446a49d1105d137974db
JakesProjects1 <jacob@kagon.net>
2020-11-08 16:40:16 -0500

    added some validations to users model

--- meme_ya_later/app/models/user.rb
+++ meme_ya_later/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    validates :name, :username, presence: true 
+    
+    
     has_many :sent_messages, class_name: "Message", foreign_key: :sender_id 
     has_many :receivers, through: :sent_messages, source: :receiver
     has_many :received_messages, class_name: "Message", foreign_key: :receiver_id 
--- meme_ya_later/config/routes.rb
+++ meme_ya_later/config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources :messages
   resources :categories, only: [:index, :show]
   get '/', to: 'application#home', as: 'home'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   get '/welcome', to: 'sessions#welcome'
   get 'authorized', to: 'sessions#page_requires_login'
   post '/logout', to: 'sessions#destroy'
